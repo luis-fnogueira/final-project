de5bd0901da6728972851035e720dd2a79d6544d
Luis Felipe <luis.felipe32971@gmail.com>
2022-07-16 21:26:06 -0300

    v2 functions

--- air/dags/functions/construction.py
+++ air/dags/functions/construction.py
@@ -1,33 +1,42 @@
 import json
 import psycopg2
 import requests
-import re
+from sqlalchemy import values
+
 
 
 class Postgres():
 
     # Constructor method initializing the connection to DB and creating cursor
-    def __init__(self, db, user, host, password, port):
+    def __init__(self, db, user, host, password, port, schema):
         
         self.db = db
         self.user = user
         self.host = host
         self.password = password
         self.port = port
+        self.schema = schema
 
         self.conn = psycopg2.connect(
             database=db, host=host, port=port, password=password, user=user)
         self.cur = self.conn.cursor()
 
     # SQL INSERT INTO method.
-    def insert_into(self, columns, values, table):
+    def insert_into(self, data, table):
 
-        postgres_insert_query = f"""INSERT INTO {self.db}.{table} {tuple(columns)} VALUES {tuple(values)}"""
+        values = tuple(data.values())
+
+        numberOfColumnsMinusOne = len(values) - 1
+        valuesParametersMinusOne = '%s, ' * numberOfColumnsMinusOne
+
+        postgres_insert_query = f"INSERT INTO {self.schema}.{table} VALUES ({valuesParametersMinusOne}%s)  "
 
         print(postgres_insert_query)
-        yield self.cur.query(postgres_insert_query)
-        self.conn.commit()
 
+        self.cur.execute(postgres_insert_query, values)
+
+        self.conn.commit()
+        
 
 
 class Bitcoin():
@@ -37,38 +46,20 @@ class Bitcoin():
 
         URL = 'https://www.mercadobitcoin.net/api/BTC/day-summary'
 
-        response_API = requests.get(f'{URL}/{year}/{month}/{day}')
+        ApiResponse = requests.get(f'{URL}/{year}/{month}/{day}')
         
-        to_json = response_API.json()
-
-        values =  []
-
-        columns = []
-
-        for column, value in to_json.items():
-            values.append(value)
-            columns.append(column)
-
-        #columns = json.dumps(columns)
-        #values = json.dumps(values)        
-
-
-        return columns, values
+        to_json = ApiResponse.json()
+        
+        return to_json
 
 if __name__ == '__main__':
     postgres = Postgres(db="bitcoin_data", user="airflow", host="localhost",
-                        password="airflow", port="5434")
+                        password="airflow", port="5434", schema='public')
 
     bitcoin = Bitcoin()
 
     response = bitcoin.response_get(2022, 7, 14)
 
-    #print(response[0], response[1])
-
     table = 'bitcoin_history'
 
-    postgres.insert_into(columns=response[0], values=response[1], table=table)
-
-    #postgres.insert_into(response[0], response[1], table)
-
-    #print(len(response[1]))
+    postgres.insert_into(response, table=table)
