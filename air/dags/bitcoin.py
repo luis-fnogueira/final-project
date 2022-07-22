97a6e9b346fec684f9d8c0c1a94708a36bc48965
Luis Felipe <luis.felipe32971@gmail.com>
2022-07-21 22:18:06 -0300

    ajusted folders

--- air/dags/bitcoin.py
+++ air/dags/bitcoin.py
@@ -1,22 +1,24 @@
-from air.functions.dag_functions.day_summary_dag import DaySummaryDag
+import sys
+# Appending our folder with functions
+sys.path.append('functions/air/dag_functions/')
+
+from functions.dag_functions.day_summary_dag import DaySummaryDag
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime, date, timedelta
-import sys
-
-# Appending our folder with functions
-sys.path.append('air/dags/functions')
 
 
 default_args = {
     'start_date': datetime(2020, 1, 1)
 }
 
-# Creating yesterday's object.
+# Creating "yesterday" object. It contains yesterday's date.
 today = date.today()
 yesterday = today - timedelta(days=1)
 
-op_args = {}
+# Initializing arguments to the task function.
+op_kwargs = {'year': yesterday.year, 'month': yesterday.month,
+            'day': yesterday.day, 'coin': 'BTC'}
 
 # Instatiating class
 btc = DaySummaryDag()
@@ -26,8 +28,7 @@ with DAG('bitcoin', schedule_interval='@daily', default_args=default_args, catch
     get_api = PythonOperator(
         task_id='get_api',
         python_callable=btc.getAndInputDaySummary,
-        op_kwargs={'year': yesterday.year, 'month': yesterday.month,
-                   'day': yesterday.day, 'coin': 'BTC'}
+        op_kwargs=op_kwargs
     )
 
     get_api
