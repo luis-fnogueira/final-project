4520ce2fe7ba6979891d9b02ee25fd8a0111b945
Luis Felipe <luis.felipe32971@gmail.com>
2022-07-25 14:52:03 -0300

    refactoring in get_bictoin folder

--- air/dags/functions/db_manipulation/postgres.py
+++ air/dags/functions/db_manipulation/postgres.py
@@ -19,65 +19,53 @@ class Postgres:
             database=db, host=host, port=port, password=password, user=user)
         self.cur = self.conn.cursor()
 
-
     # Getters of attributes
 
     @property
     def db(self):
         return self.__db
 
-
     @property
     def user(self):
         return self.__user
-
 
     @property
     def host(self):
         return self.__host
 
-
     @property
     def password(self):
         return self.__password
-
 
     @property
     def port(self):
         return self.__port
 
-
     @property
     def schema(self):
         return self.__schema
 
-        
     # Setters
 
     @db.setter
     def db(self, db):
         self.__db = db
 
-
     @user.setter
     def user(self, user):
         self.__user = user
-
 
     @host.setter
     def host(self, host):
         self.__host = host
 
-
     @password.setter
     def password(self, password):
         self.__password = password
 
-
     @port.setter
     def port(self, port):
         self.__port = port
-
 
     @schema.setter
     def schema(self, schema):
--- air/dags/functions/get_bitcoin/mercado_bitcoin.py
+++ air/dags/functions/get_bitcoin/mercado_bitcoin.py
@@ -1,4 +1,4 @@
-from functions.get_bitcoin.get_response import GetResponse
+from get_response import GetResponse
 
 
 class MercadoBitcoin(GetResponse):
@@ -6,21 +6,21 @@ class MercadoBitcoin(GetResponse):
     # Initializing the class with the standard URL and the class GetResponse
     def __init__(self) -> None:
 
-        self.beginningOfUrl = 'https://www.mercadobitcoin.net/api'
+        self.__ENDPOINT = 'https://www.mercadobitcoin.net/api'
         GetResponse.__init__(self)
 
     # This method is to get generally from the API
-    def standardGet(self, coin: str, method: str):
+    def standardGet(self, coin: str, method: str) -> dict:
 
-        self.URL = f'{self.beginningOfUrl}/{coin}/{method}/'
+        self.URL = f'{self.__ENDPOINT}/{coin}/{method}/'
 
         return self.getDeserialize(url=self.URL)
 
     # This method is to get specifically from the API Day Summary
-    def daySummary(self, year: int, month: int, day: int, coin: str):
+    def daySummary(self, year: int, month: int, day: int, coin: str) -> dict:
 
         METHOD = 'day-summary'
 
-        self.URL = f'{self.beginningOfUrl}/{coin}/{METHOD}/{year}/{month}/{day}'
+        self.URL = f'{self.__ENDPOINT}/{coin}/{METHOD}/{year}/{month}/{day}'
 
         return self.getDeserialize(url=self.URL)
