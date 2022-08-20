c81b0665b09446177853df1f71290e05cb58415e
Luis Felipe Almeida Nogueira <82065199+luis-fnogueira@users.noreply.github.com>
2022-08-20 18:52:21 -0300

    Update create_table.sql

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -10,7 +10,7 @@ CREATE TABLE bitcoin_history
     closing DECIMAL(14, 8),
     lowest DECIMAL(14, 8),
     highest DECIMAL(14, 8),
-    volume DECIMAL(16, 8),
+    volume DECIMAL(17, 8),
     quantity DECIMAL(16, 8),
     amount INT(5),
     avg_price DECIMAL(16, 8)
